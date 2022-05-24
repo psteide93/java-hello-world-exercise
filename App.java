669e6172839f901663b022dec5c3b13da6b5d575
Parnell <psteide@ford.com>
2022-05-24 07:48:36 -0400

    complied code

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
-class App {
-  public static void main(String args[]){
+public class App {
+  public static void main(String[] args){
     String message = "Hello, world!";
     System.out.println(message);
   }
